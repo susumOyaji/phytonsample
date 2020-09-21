13515293973ef82f61064f1a75953c8258f59dab
susumOyaji <sumitomo0210@gmail.com>
2020-09-21 13:29:33 +0900

    up

--- demo.py
+++ demo.py
@@ -8,13 +8,15 @@ import numpy as np
 
 #%matplotlib inline
 #import matplotlib.pyplot as plt
+import time
+from selenium import webdriver
+ 
 
 
 
 
-
-driver = webdriver.Chrome()
-driver.get('https://www.google.com/')
+driver = webdriver.Chrome()#driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+#driver.get('https://www.google.com/')
 #time.sleep(5)
 #search_box = driver.find_element_by_name("q")
 #search_box.send_keys('ChromeDriver')
