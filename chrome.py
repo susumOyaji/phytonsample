45a8e67d126a652d7273461a421ae4d1642b8186
susumOyaji <sumitomo0210@gmail.com>
2020-09-25 17:47:50 +0900

    up

--- chrome.py
+++ chrome.py
@@ -8,7 +8,7 @@ from selenium import webdriver
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome('/path/to/chromedriver')  # Optional argument, if not specified will search path.
+driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
 driver.get('http://www.google.com/');
 time.sleep(5) # Let the user actually see something!
 search_box = driver.find_element_by_name('q')
@@ -32,7 +32,7 @@ search_box.send_keys('Selenium')
 search_box.submit()
 
 # 検索結果からタイトルが「Selenium - Web Browser Automation」のリンクをクリックする。
-driver.find_element_by_link_text("Selenium - Web Browser Automation").click()
+driver.find_element_by_link_text("Webの操作を自動化してくれる「Selenium」を使ってみた ...").click()
 
 # 5秒待つ
 time.sleep(5)
