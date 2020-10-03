caf912325a2ed530e9ddf299acd7c081d00dd266
susumOyaji <sumitomo0210@gmail.com>
2020-10-03 13:45:45 +0900

    up

--- web.py
+++ web.py
@@ -21,7 +21,7 @@ def get_htmls(stock_number):
   print(head[0])#ソニー（株）
   data = [d.text for d in tag_tr[0].find_all('td')]
   print('stoksPrice: '+data[1])
-  print(data[2])
+  #print(data[2])
   #data = []
   #for i in range(1,len(tag_tr)):
     #data.append([d.text for d in tag_tr[i].find_all('td')])
@@ -32,33 +32,62 @@ def get_htmls(stock_number):
 
 
 
-def get_hiprice():
+def get_Year_to_date_highs():
   urlName ='https://info.finance.yahoo.co.jp/ranking/?kd=29&mk=3&tm=d&vl=a'#年初来高値
-  #https://info.finance.yahoo.co.jp/ranking/?kd=1&mk=3&tm=d&vl=a値下がり率
   soup = BeautifulSoup(requests.get(urlName).content, 'html.parser')
   text=soup.get_text()#.get_text()は、テキストのみを取得する、つまりタグは取らないメソッドです。
-  #print(text)
-  print(soup)  
+  
+  data1 = [d.text for d in soup.find_all('td')]
+  #for i in range(1, len(data1)):
+  #  print('年初来高値: '.decode('utf-8')+data1[i])
+  return data1
 
-  tag_tr = soup.find_all('tr')
-  print(tag_tr[0])
+def  Price_drop():
+  urlName = 'https://info.finance.yahoo.co.jp/ranking/?kd=1&mk=3&tm=d&vl=a'#値下がり率
+  soup = BeautifulSoup(requests.get(urlName).content, 'html.parser')
+  text = soup.get_text()  #.get_text()は、テキストのみを取得する、つまりタグは取らないメソッドです。
+  data2 = [d.text for d in soup.find_all('td')]
+  #for i in range(1,len(data2)):
+  #  #print('値上がり率: '.decode('utf-8') + data2[i])
+  return data2
 
-  head = [h.text for h in tag_tr[0].find_all('th')]
-  print(head[0])#ソニー（株）
-  data = [d.text for d in tag_tr[0].find_all('td')]
-  print('stoksPrice: '+data[0])
-  print(data[2])
+def Volume_per_unit():
+  urlName ='https://info.finance.yahoo.co.jp/ranking/?kd=32&mk=3&tm=d&vl=a'#単元当たり出来高  
+  soup = BeautifulSoup(requests.get(urlName).content, 'html.parser')
+  text = soup.get_text()  #.get_text()は、テキストのみを取得する、つまりタグは取らないメソッドです。
+  data3 = [d.text for d in soup.find_all('td')]
+  #for i in range(1,len(data3)):
+    #print('単元当たり出来高: '.decode('utf-8') + data3[i])
+  return data3
 
-stack_code = ['6758','6976','4755']
+
+def Stop_High():
+  urlName = 'https://info.finance.yahoo.co.jp/ranking/?kd=27&mk=3&tm=d&vl=a'#ストップ高  
+  soup = BeautifulSoup(requests.get(urlName).content, 'html.parser')
+  text = soup.get_text()  #.get_text()は、テキストのみを取得する、つまりタグは取らないメソッドです。
+  data4 = [d.text for d in soup.find_all('td')]
+  #for i in range(1,len(data4)):
+    #print('ストップ高: '.decode('utf-8') + data4[i])
+  return data4
+  
+
+
+
+stack_code = ['6758', '6976', '4755']
+data1 = ''
 
 #複数のデータフレームをcsvで保存
 #for i in stack_code:
   #get_htmls(i)
-get_hiprice() 
+highs = get_Year_to_date_highs()
+price = Price_drop()
+volume = Volume_per_unit()
+stop = Stop_High()
 
-
-
-
+print('年初来高値: '.decode('utf-8')+highs[2]+'¥'.decode('utf-8')+highs[7])
+print('値上がり率: '.decode('utf-8')+price[3]+'¥'.decode('utf-8')+price[7])
+print('単元当たり出来高: '.decode('utf-8')+volume[3]+'¥'.decode('utf-8')+volume[5]+'   '+'¥'.decode('utf-8')+volume[8])
+print('ストップ高: '.decode('utf-8') + stop[2]+'¥'.decode('utf-8')+stop[6])
 
 
 
