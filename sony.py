9bfaa203d7db4adae2d2bbcca1d5cfca8c7a3c01
susumOyaji <sumitomo0210@gmail.com>
2020-10-31 11:30:34 +0900

    up

--- sony.py
+++ sony.py
@@ -1,4 +1,4 @@
-import ConfigParser
+import configparser
 import urllib
 import sqlite3
 import datetime
@@ -24,7 +24,7 @@ def learn_db_init():
       ltday.append(row[0]);
    conn.close();
 
-def rnd_forest(ltree_scode,tscode,tdate):
+def rnd_forest(ltree_scode,tscode,tdate):#############
 
    conn = sqlite3.connect("chart.db");
    c = conn.cursor();
@@ -38,7 +38,7 @@ def rnd_forest(ltree_scode,tscode,tdate):
       if ltday[i]==tdate:
          dp=i;
          break;
-   if dp-102 &lt:#0:
+   if dp-102 < 0:  #&lt:#0:
       return 0;
 
    whe="";
@@ -59,10 +59,10 @@ def rnd_forest(ltree_scode,tscode,tdate):
       for row in c:
          lr[row[0]]=row[1];      
 
-      if d &gt; dp-102:      
+      if d > dp-102:      
          lx=[];
          for s in ltree_scode:
-            if lr[s] &lt; lbr[s]:
+            if lr[s] < lbr[s]:
                lx.append(-1);
             else:
                lx.append(1);   
@@ -73,7 +73,7 @@ def rnd_forest(ltree_scode,tscode,tdate):
             ltra_x.append(lx); 
 
       for s in ltree_scode:
-         if lr[s] &gt; 0:
+         if lr[s] > 0:
             lbr[s]=lr[s];
 
 
@@ -83,7 +83,7 @@ def rnd_forest(ltree_scode,tscode,tdate):
    r=-1;
    for row in c:
       if r>0:
-         if r &lt; row[1]:
+         if r < row[1]:
             ltra_y.append(1);
          else:
             ltra_y.append(-1);
