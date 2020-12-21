992cc4498dd95f48cbd00b3b7f8511eefe2b1bb1
susumOyaji <sumitomo0210@gmail.com>
2020-12-21 15:14:20 +0900

    up

--- search_stock.py
+++ search_stock.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import time
 import datetime
 from selenium import webdriver
-import chromedriver_binary
+#import chromedriver_binary
 
 
 def sample():
@@ -13,12 +13,34 @@ def sample():
   
 def get_stock_list(low, high, target_code):
   print('get_stock_list to get_stock_list()')
-  driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
+  driver = webdriver.Chrome("c:/Users/chromedriver_win32/chromedriver.exe")  # Optional argument, if not specified will search path.
   driver.get('https://stock.web0000.jp/') 
   time.sleep(5) # Let the user actually see something!
   
   
-  month_elem = driver.find_element_by_id("input-57")
+  #month_elem = driver.find_element_by_id("__nuxt")
+  #fruit = driver.find_element_by_class_name('v-text-field__slot')
+  month_elem = driver.find_element_by_id("input-47")
   month_elem.clear()
   month_elem.send_keys("12")
+
+  month_elem = driver.find_element_by_id("input-50")
+  month_elem.clear()
+  month_elem.send_keys("24")
+
+  month_elem = driver.find_element_by_id("input-53")
+  month_elem.clear()
+  month_elem.send_keys("6758")
+
+  driver.findElement(By.type("button")).click();
   
+
+  # 一番最初の「りんご」が返却される
+  fruit = driver.find_element_by_class_name('fruit')
+  # 一番最初の「りんご」「メロン」「桃」の全てがリストで返却される
+  fruits = driver.find_elements_by_class_name('fruit')
+  username = driver.find_element_by_name('username')
+  password = driver.find_element_by_name('password')
+
+  #このような条件が複雑な指定方法では find_element_by_css_selector を使うと良いでしょう。 ここではclass属性の親子関係関係に着目して要素を絞り込みます。
+  apple = driver.find_element_by_css_selector('.menu .apple')
