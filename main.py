5d5a51bddf909a74db37b55f6c12b30a825005d5
susumOyaji <sumitomo0210@gmail.com>
2021-03-03 11:08:53 +0900

    up

--- main.py
+++ main.py
@@ -24,6 +24,19 @@ Window.clearcolor = (0.5, 0.5, 0.5, 1)
 Window.size = (900, 1320)
 
 
+def get_dowhtmls():
+  urlName = 'https://finance.yahoo.co.jp/quote/%5EDJI'
+  soup = BeautifulSoup(requests.get(urlName).content, 'html.parser')
+  
+  tag_tr = soup.find_all('dd')#tr
+  #print(tag_tr[0])
+  head = [h.text for h in tag_tr[1].find_all('span')]#th
+  data = head[0]
+  return data
+
+
+
+
 
 def get_htmls(stock_number):
   urlName = "https://stocks.finance.yahoo.co.jp/stocks/detail/?code="+stock_number
@@ -49,56 +62,9 @@ def get_htmls(stock_number):
 
 
 
-
-class user(Screen):
-    def add_more(self):
-        self.ids.rows.add_row()
-
-
-    #Newyork dow
-
-    #Nikkei25
-
-    #MyItem
-    #企業コード nikkei,sony,taiyou,rukten
-    code = ['998407','6758', '6976', '4755'] 
-    price = ['0','1665', '1801', '1137'] #購入単価
-    quantity = [1, 200, 300, 400] #数量
-    
-    Marketprice = [] #個別時価総額=数量ｘ時価
-    name = [] #企業名
-    price = [] #購入単価
-   
-    for i in code:
-        responce = get_htmls(i)
-        name.append(responce[0])
-        price.append(responce[1])
-
-    
-    for i in range(len(code)):
-        try:
-            Marketprice.append(float(price[i].replace(',', '')) * quantity[i])
-        except ZeroDivisionError:
-            print('You can not do this operation!')
-   
-    #Label1
-    seconds_string = '  Stack Card'
-
-    #Label2
-    newyork = 'NewYork Dow \n$' + ' In earnest program'
-    
-    #Label3
-    nikei225 = name[0] + '\n¥' + price[1] + '\n¥' + str(Marketprice[1])
-
-    #Label4
-    rakuten = name[3] + '\n¥' + price[3] + '\n¥' + str(Marketprice[3])
-
-
-
-
 class Row(BoxLayout):
     button_text = StringProperty("")
-   
+    item_value = StringProperty("")
 
 
 class Rows(BoxLayout):
@@ -108,14 +74,75 @@ class Rows(BoxLayout):
         super(Rows, self).__init__(**kwargs)
         self.add_row()
 
+
     def add_row(self):
         self.row_count += 1
-        self.add_widget(Row(button_text=str(self.row_count)))
+        self.add_widget(Row(button_text=str(self.row_count),item_value=str(self.row_count)))
     
+
     def remove_row(self):
         if self.content.children:
             self.content.remove_widget(self.content.children[0])
             self.row_count -= 1
+
+
+
+class user(Screen):
+    #Newyork dow
+    dow = get_dowhtmls()
+    #Label2
+    newyork = 'NewYork Dow $' + dow
+    #Nikkei25
+    nikkei= get_htmls('998407')
+    #Label3
+    nikei225 = nikkei[0] + '   ¥' + nikkei[1] #+ '\n¥' + str(Marketprice[0])
+    #MyItem
+    #企業コード nikkei,sony,taiyou,rukten
+    code = ['6758', '6976', '4755'] 
+    price = [1665, 1801, 1137] #購入単価
+    quantity = [200, 300, 400] #数量
+    
+    Marketprice = [] #個別時価総額=数量ｘ時価
+    name = [] #企業名
+    value = [] #時価
+    TotalValue = 0
+
+
+    def add_more(self):
+        self.ids.rows.add_row()
+
+
+    for i in code:
+        responce = get_htmls(i)
+        name.append(responce[0])
+        value.append(responce[1])
+        
+    
+        
+    #Label1
+    seconds_string = '  Stack Card'
+    
+    for i in range(len(code)):
+        try:
+            Marketprice.append(float(value[i].replace(',', '')) * quantity[i])
+            TotalValue = TotalValue + Marketprice[i]
+            add_more(self)
+        except ValueError:
+            newyork = '---'
+            nikei225 = '---'
+            rakuten='---'
+            #print('You can not do this operation!')
+    #Label4
+    TotalAsset= 'TotalAsset   ¥'+str("{:,}".format(TotalValue))
+    rakuten = name[2] + '\n¥' + str(price[2]) + '\n¥' + str(Marketprice[2])
+              
+    inst = Rows()
+    inst.add_row()
+    
+
+
+
+
 
 
 
