19ce4ade0d8addb8bc3e1ff460fe32cea820f53d
susumOyaji <sumitomo0210@gmail.com>
2021-03-09 17:49:15 +0900

    up

--- main.py
+++ main.py
@@ -22,6 +22,16 @@ Config.set('graphics', 'top',  20)
 #Window.clearcolor = (0.5, 0.5, 0.5, 1)
 Window.size = (900, 1320)
 
+#企業コード nikkei,sony,taiyou,rukten
+code = ['6758', '6976', '4755'] 
+price = [1665, 1801, 1137] #購入単価
+quantity = [200, 300, 400] #数量
+Marketprice = [] #個別時価総額=数量ｘ時価
+value = [] #時価
+name = [] #企業名
+before = []
+TotalValue = 0
+
 
 def get_dowhtmls():
   urlName = 'https://finance.yahoo.co.jp/quote/%5EDJI'
@@ -29,7 +39,7 @@ def get_dowhtmls():
   
   tag_tr = soup.find_all('dd')#tr
   #print(tag_tr[0])
-  head = [h.text for h in tag_tr[1].find_all('span')]#th
+  head = [h.text for h in tag_tr[3].find_all('span')]#th
   data = head[0]
   return data
 
@@ -57,49 +67,6 @@ def get_htmls(stock_number):
   return data
 
 
-def item_add(self):
-    #Newyork dow
-    dow = get_dowhtmls()
-    #Label2
-    newyork = 'NewYork Dow $' + dow
-    #Nikkei25
-    nikkei= get_htmls('998407')
-    #Label3
-    nikei225 = nikkei[0] + '   ¥' + nikkei[1] #+ '\n¥' + str(Marketprice[0])
-    #MyItem
-    #企業コード nikkei,sony,taiyou,rukten
-    code = ['6758', '6976', '4755'] 
-    price = [1665, 1801, 1137] #購入単価
-    quantity = [200, 300, 400] #数量
-
-    Marketprice = [] #個別時価総額=数量ｘ時価
-    name = [] #企業名
-    value = [] #時価
-    TotalValue = 0
-
-    for i in code:
-        responce = get_htmls(i)
-        name.append(responce[0])
-        value.append(responce[1])
-        
-    for i in range(len(code)):
-        try:
-        
-    
-            Marketprice.append(float(value[i].replace(',', '')) * quantity[i])
-            TotalValue = TotalValue + Marketprice[i]
-            
-           
-
-        except ValueError:
-            Marketprice.append('---'); newyork = '---'; nikei225 = '---';rakuten='---'
-        
-    #Label4
-    TotalAsset= 'TotalAsset   ¥'+str("{:,}".format(TotalValue))
-    rakuten = name[2] + '\n¥' + str(price[2]) + '\n¥' + str(Marketprice[2])
-
-    self.root.ids.rows.add_row()
-    self.root.ids.rows.add_row()
 
 
 
@@ -111,28 +78,46 @@ class Row(BoxLayout):
 
 class Rows(BoxLayout):
     row_count = 0
+    
+    
+
+    for i in code:
+        responce = get_htmls(i)
+        name.append(responce[0])
+        value.append(responce[1])
+        before.append(responce[2])
+
 
     def __init__(self, **kwargs):
         super(Rows, self).__init__(**kwargs)
        # self.row_count = 0
-        self.add_row()
+        self.add_code()
         
     def add_row(self):
         self.row_count += 1
         self.add_widget(Row(button_text=str(self.row_count),item_value=str(self.row_count)))
+
+    def add_code(self):
+        for i in range(len(code)):
+            self.row_count += 1
+            self.add_widget(Row(button_text=str(self.row_count),item_value=name[i]+'\n'+ value[i] + before[i]))
     
-    '''
+
+
+
+
+'''
     def remove_row(self):
         if self.content.children:
             self.content.remove_widget(self.content.children[0])
             self.row_count -= 1
+'''
 
 
 
 
 
-
-
+'''
 KvLanguageのclass ruleとroot ruleの違いを説明させて下さい。
 まずclass rule("<クラス名>:"で始まるもの)は
 型の定義であり、そこに書いた物はその型のインスタンス全てに影響を与えます(厳密には少し違いますが)。
@@ -149,6 +134,7 @@ class user(Screen):
     def add_more(self):#Add Button to Push
         self.ids.rows.add_row()
 
+
     #Newyork dow
     dow = get_dowhtmls()
     #Label2
@@ -157,41 +143,20 @@ class user(Screen):
     nikkei= get_htmls('998407')
     #Label3
     nikei225 = nikkei[0] + '   ¥' + nikkei[1] #+ '\n¥' + str(Marketprice[0])
-    #MyItem
-    #企業コード nikkei,sony,taiyou,rukten
-    code = ['6758', '6976', '4755'] 
-    price = [1665, 1801, 1137] #購入単価
-    quantity = [200, 300, 400] #数量
+    
 
-    Marketprice = [] #個別時価総額=数量ｘ時価
-    name = [] #企業名
-    value = [] #時価
-    TotalValue = 0
-  
-
-
-
-    for i in code:
-        responce = get_htmls(i)
-        name.append(responce[0])
-        value.append(responce[1])
-        
     for i in range(len(code)):
         try:
-        
-    
             Marketprice.append(float(value[i].replace(',', '')) * quantity[i])
             TotalValue = TotalValue + Marketprice[i]
-        
-           
-
         except ValueError:
-            Marketprice.append('---'); newyork = '---'; nikei225 = '---';rakuten='---'
+            Marketprice.append('---'); newyork = '---'; nikei225 = '---'
         
     #Label4
     TotalAsset= 'TotalAsset   ¥'+str("{:,}".format(TotalValue))
-    rakuten =''# name[2] + '\n¥' + str(price[2]) + '\n¥' + str(Marketprice[2])
-   
+    
+    
+    
    
 
 
@@ -205,7 +170,6 @@ class Test1(App):
         self.root = Builder.load_file('floatlayout.kv')
         self.title = 'Python to Iphone App'
         
-        anser = item_add(self)
 
         return self.root
 
