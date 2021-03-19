2d50535931bd28aa4517220696580d7f29d33747
susumOyaji <sumitomo0210@gmail.com>
2021-03-19 17:42:11 +0900

    up

--- main.py
+++ main.py
@@ -198,14 +198,14 @@ class Rows(BoxLayout):
         for i in range(len(code)):
             self.row_count += 1
             self.add_widget(Row(button_text=str(self.row_count),item_value=name[i]+'\n'+ value[i],item_ratio= before[i]))
-        
-        #self.rv.data = []
+        '''
+        self.rv.data = []
         btn_list = ['ひつまぶし','味噌煮込みうどん','味噌カツ','台湾ラーメン' \
                     ,'手羽先','小倉トースト','きしめん','あんかけスパ','どて煮' \
                     ,'ういろう','甘口バナナスパ']
         for btn_list_any in btn_list:
             self.rv.data.append({'value': btn_list_any})
-    
+        '''
     
     
     
@@ -286,6 +286,8 @@ class VariousButtons(BoxLayout):
     def on_select_button(self, button):
         print('press:' + button.text)
 
+
+
 class Test(BoxLayout):
     def __init__(self, **kwargs):
         super(Test, self).__init__(**kwargs)
@@ -307,7 +309,7 @@ class Test1(App):
     def build(self):
         #self.root = Builder.load_file('Demo.kv')
         #self.root = Builder.load_file('floatlayout.kv')
-        #self.title = 'Python to Iphone App'
+        self.title = 'Python to Iphone App'
         return Test() #self.root
 
 
