52f6b04d8948951bbca2c2e088dbfc99d7b395de
susumOyaji <sumitomo0210@gmail.com>
2021-03-20 10:10:00 +0900

    up

--- Rv.py
+++ Rv.py
@@ -9,6 +9,56 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.recycleboxlayout import RecycleBoxLayout
 
 
+from bs4 import BeautifulSoup
+import requests
+import japanize_kivy
+
+
+#企業コード nikkei,sony,taiyou,rukten
+code = ['6758', '6976', '4755'] 
+price = [1665, 1801, 1137] #購入単価
+quantity = [200, 300, 400] #数量
+Marketprice = [] #個別時価総額=数量ｘ時価
+value = [] #時価
+name = [] #企業名
+before = []
+ratio = []
+TotalValue = 0
+
+
+def get_dowhtmls():
+  urlName = 'https://finance.yahoo.co.jp/quote/%5EDJI'
+  soup = BeautifulSoup(requests.get(urlName).content, 'html.parser')
+  
+  tag_tr = soup.find_all('dd')#tr
+  #print(tag_tr[0])
+  head = [h.text for h in tag_tr[3].find_all('span')]#th
+  data = head[0]
+  return data
+
+
+
+
+
+def get_htmls(stock_number):
+  urlName = "https://stocks.finance.yahoo.co.jp/stocks/detail/?code="+stock_number
+  soup = BeautifulSoup(requests.get(urlName).content, 'html.parser')
+  #text=soup.get_text()#.get_text()は、テキストのみを取得する、つまりタグは取らないメソッドです。
+  
+  tag_tr = soup.find_all('tr')
+  #print(tag_tr[0])
+
+  head = [h.text for h in tag_tr[0].find_all('th')]
+  #print(head[0])#ソニー（株）
+  data = [d.text for d in tag_tr[0].find_all('td')]
+  data[0] = head[0]
+ 
+  print('Name: ',data[0])
+  print('StoksPrice: ',data[1])
+  print('The day before ratio: ',data[2])
+  print('')
+  return data
+
 class AppRoot(BoxLayout):
     pat = re.compile(r'\.{3}')
     my_rv = ObjectProperty()
@@ -19,6 +69,36 @@ class AppRoot(BoxLayout):
     def create_data(self, btn):
         data_txt = self.pat.sub(self.ids.input.text.capitalize(), btn.text)
         self.my_rv.data.append({'text': data_txt})  # 2:
+
+    
+    for i in code:
+        responce = get_htmls(i)
+        name.append(responce[0])
+        value.append(responce[1])
+        #before.append(responce[2]) #前日比
+        ratio = responce[2].replace('前日比','')
+        before.append(ratio)    
+
+    #Newyork dow
+    dow = get_dowhtmls()
+    #Label2
+    newyork = 'NewYork Dow $' + dow
+    #Nikkei25
+    nikkei= get_htmls('998407')
+    #Label3
+    nikei225 = nikkei[0] + '   ¥' + nikkei[1] #+ '\n¥' + str(Marketprice[0])
+    
+
+    for i in range(len(code)):
+        try:
+            Marketprice.append(float(value[i].replace(',', '')) * quantity[i])
+            TotalValue = TotalValue + Marketprice[i]
+        except ValueError:
+            Marketprice.append('---'); newyork = '---'; nikei225 = '---'
+        
+    #Label4
+    TotalAsset= 'TotalAsset   ¥'+str("{:,}".format(TotalValue))
+
 
 
 class MyRecycleBoxLayout(RecycleBoxLayout):
