675b226fbd913d5c2609313e5be2b4ab54e7063e
susumOyaji <sumitomo0210@gmail.com>
2021-04-06 15:31:23 +0900

    up

--- MainScreen.py
+++ MainScreen.py
@@ -5,14 +5,31 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.lang import Builder
+from bs4 import BeautifulSoup
+import requests
 #import japanize_kivy
+
+
+
+#企業コード nikkei,sony,taiyou,rukten
+code = ['6758', '6976', '4755'] 
+price = [1665, 1801, 1137] #購入単価
+quantity = [200, 300, 400] #数量
+Marketprice = [] #個別時価総額=数量ｘ時価
+value = [] #時価
+name = [] #企業名
+before = []
+ratio = []
+TotalValue = 0
+
+
 
 kv = """
 #:import hex kivy.utils.get_color_from_hex
 <MainScreen>:
+    rv: rv
     BoxLayout: #All-Screen
         orientation: 'vertical'
-        rv: rv
         #size_hint_y: 1.0
         canvas.before:
             Color:
@@ -169,52 +186,162 @@ kv = """
 """
 Builder.load_string(kv)
 
+def get_htmls(stock_number):
+  data = []
+
+  urlName = "https://finance.yahoo.co.jp/quote/"+stock_number
+  soup = BeautifulSoup(requests.get(urlName).content, 'html.parser')
+  #text=soup.get_text()#.get_text()は、テキストのみを取得する、つまりタグは取らないメソッドです。
+  
+  tag_tr = soup.find_all('body')
+  #print(tag_tr[0])
+
+  head = [h.text for h in tag_tr[0].find_all('span')]
+  name = [h.text for h in tag_tr[0].find_all('h1')]
+  #print(head[0])#ソニー（株）
+   
+  data.append(name[1])
+  data.append(head[22])
+  data.append(head[29])
+ 
+  print('Name: ',data[0])
+  print('StoksPrice: ',data[1])
+  print('The day before ratio: ',data[2])
+  print('')
+  return data
+
+
+
+def get_dowhtmls():
+  urlName = 'https://finance.yahoo.co.jp/quote/%5EDJI'
+  soup = BeautifulSoup(requests.get(urlName).content, 'html.parser')
+  
+  tag_tr = soup.find_all('dd')#tr
+  #print(tag_tr[0])
+  head = [h.text for h in tag_tr[3].find_all('span')]#th
+  data = head[0]
+  return data
+
+
+
+def get_nikkeyhtmls():
+  data = []  
+  #urlName = 'https://stocks.finance.yahoo.co.jp/stocks/detail/?code=998407.O'
+  urlName = 'https://stocks.finance.yahoo.co.jp/stocks/detail/?code=998407.O'
+  soup = BeautifulSoup(requests.get(urlName).content, 'html.parser')
+  
+  tag_tr = soup.find_all('body')#tr
+  #print(tag_tr[0])
+  head = [h.text for h in tag_tr[0].find_all('td')]  #tr
+  
+  value = head[1]
+  ratio = head[2]
+  name = head[3]
+  
+  data.append(name)
+  data.append(value)
+  data.append(ratio)
+  return data
+
+
+def get_htmls(stock_number):
+  data = []
+
+  urlName = "https://finance.yahoo.co.jp/quote/"+stock_number
+  soup = BeautifulSoup(requests.get(urlName).content, 'html.parser')
+  #text=soup.get_text()#.get_text()は、テキストのみを取得する、つまりタグは取らないメソッドです。
+  
+  tag_tr = soup.find_all('body')
+  #print(tag_tr[0])
+
+  head = [h.text for h in tag_tr[0].find_all('span')]
+  name = [h.text for h in tag_tr[0].find_all('h1')]
+  #print(head[0])#ソニー（株）
+   
+  data.append(name[1])
+  data.append(head[22])
+  data.append(head[29])
+ 
+  print('Name: ',data[0])
+  print('StoksPrice: ',data[1])
+  print('The day before ratio: ',data[2])
+  print('')
+  return data
+
 
 class MainScreen(BoxLayout):#画面上の見た目や機能を構成するクラス
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-        
         self.orientation = "vertical"#horizontal"
-        '''
-        btn = Button(text="hello")
-        self.add_widget(btn)
+        self.rv.data = []
+                  
+        for name_list_any in name:
+            self.rv.data.append({'value': name_list_any})
 
-        btn2 = Button(text="everyone")
-        self.add_widget(btn2)
-
-
-        # MainScreenに追加する、BoxLayoutというWidgetを用意
-        bl = BoxLayout()
-        bl.orientation = "vertical"
-
-        # blに追加する３つのボタンを用意
-        lbl3 = Label(text="how")
-        lbl3.size_hint_y = 0.5
-        lbl3.size_hint_x = 0.5
-        lbl3.color = 1, 0, 0, 1#text color
-        lbl3.bold = True
-        lbl3.font_size = 30
-        lbl3.bg='#F0F8FF'
-        # 色を設定
-        #lbl3['bg'] = '#F0F8FF'
-        #lbl3.config(bg='#F0F8FF')
-
-
-        lbl4 = Label(text="are")
-        lbl5 = Label(text="you")
+    for i in code:
+        responce = get_htmls(i)
+        name.append(responce[0])
+        value.append(responce[1])
+        ratio = responce[2].replace('前日比','')
+        before.append(ratio)        
+    #Newyork dow
+    dow = get_dowhtmls()
+    #Label2
+    newyork = 'NewYork Dow $' + dow
+    #Nikkei25
+    nikkei= get_nikkeyhtmls()
+    #Label3
+    nikei225 = nikkei[0] + nikkei[1] + '\n' + nikkei[2]
+    
+    for i in range(len(code)):
+        try:
+            Marketprice.append(float(value[i].replace(',', '')) * quantity[i])
+            TotalValue = TotalValue + Marketprice[i]
+        except ValueError:
+            Marketprice.append('---')
         
-        # blにボタンを追加
-        bl.add_widget(lbl3)     
-        bl.add_widget(lbl4)
-        bl.add_widget(lbl5)
+    #Label4
+    TotalAsset= 'TotalAsset   ¥'+str("{:,}".format(TotalValue))    
+        
+'''
+btn = Button(text="hello")
+self.add_widget(btn)
 
-        # MainScreenにblを追加
-        self.add_widget(bl)
+btn2 = Button(text="everyone")
+self.add_widget(btn2)
 
-        btn6 = Button(text="today?")
-        self.add_widget(btn6)
-        '''
+
+# MainScreenに追加する、BoxLayoutというWidgetを用意
+bl = BoxLayout()
+bl.orientation = "vertical"
+
+# blに追加する３つのボタンを用意
+lbl3 = Label(text="how")
+lbl3.size_hint_y = 0.5
+lbl3.size_hint_x = 0.5
+lbl3.color = 1, 0, 0, 1#text color
+lbl3.bold = True
+lbl3.font_size = 30
+lbl3.bg='#F0F8FF'
+# 色を設定
+#lbl3['bg'] = '#F0F8FF'
+#lbl3.config(bg='#F0F8FF')
+
+
+lbl4 = Label(text="are")
+lbl5 = Label(text="you")
+
+# blにボタンを追加
+bl.add_widget(lbl3)     
+bl.add_widget(lbl4)
+bl.add_widget(lbl5)
+
+# MainScreenにblを追加
+self.add_widget(bl)
+
+btn6 = Button(text="today?")
+self.add_widget(btn6)
+'''
 
 class MainApp(App):#アプリを構成するクラス
     def on_start(self):
@@ -222,6 +349,7 @@ class MainApp(App):#アプリを構成するクラス
 
     def build(self):
         MS = MainScreen()
+        self.title = 'Python to Iphone App'
         return MS
 
     def on_stop(self):
