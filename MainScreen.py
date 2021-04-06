43f4339c948830a9085e512946e2ee5912b9cc57
susumOyaji <sumitomo0210@gmail.com>
2021-04-06 14:27:50 +0900

    up

--- MainScreen.py
+++ MainScreen.py
@@ -6,27 +6,15 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.lang import Builder
 
-
 kv = """
+#:import hex kivy.utils.get_color_from_hex
 <MainScreen>:
-    #BoxLayout:
-        #orientation: 'vertical'
-    Label:
-        #text_size: self.size
-        #halign: 'right'
-        #valign: 'middle'
-        pos_hint:{ 'center_x': .5,'center_y': .5}
-        #padding: 0.0, 5.0 #左右、上
-        #spacing: 100, 100 #        
-        text: 'Stock Card'
-        size_hint_y: 1.0
-        size_hint_x: 0.5
-        color: 1, 0, 0, 1#text color
-        bold: True
-        font_size: 30
+    BoxLayout: #All-Screen
+        orientation: 'vertical'
+        size_hint_y: 0.3
         canvas.before:
             Color:
-                rgba: 1, 1, 1, 1
+                rgba: hex('#ff0606')
             Line:
                 rectangle: self.x+1,self.y+1,self.width-1,self.height-1
                 dash_offset: 5
@@ -34,44 +22,99 @@ kv = """
             #Rectangle:
             #    pos: self.pos
             #    size: self.size
-    #BoxLayout:
-        #orientation: 'vertical'
-    Label:
-        text: 'Stock Card'
-        #size_hint_y: 0.1
-        #size_hint_x: 0.3
-        color: 1, 0, 0, 1#text color
-        bold: True
-        font_size: 30
-        canvas.before:
-            Color:
-                rgba: 1, 1, 1, 1
-            Line:
-                rectangle: self.x+1,self.y+1,self.width-1,self.height-1
-                dash_offset: 5
-                dash_length: 3
-            #Rectangle:
-            #    pos: self.pos
-            #    size: self.size
-    #BoxLayout:
-        #orientation: 'vertical'
-    Label:
-        text: 'Stock Card'
-        size_hint_y: 0.1
-        size_hint_x: 0.3
-        color: 1, 0, 0, 1#text color
-        bold: True
-        font_size: 30
-        canvas.before:
-            Color:
-                rgba: 1, 1, 1, 1
-            Line:
-                rectangle: self.x+1,self.y+1,self.width-1,self.height-1
-                dash_offset: 5
-                dash_length: 3
-            #Rectangle:
-            #    pos: self.pos
-            #    size: self.size      
+        BoxLayout: #Stack-Card
+            orientation: 'horizontal'
+            canvas.before:
+                Color:
+                    rgba: hex('#ffff06')
+                Line:
+                    rectangle: self.x+1,self.y+1,self.width-1,self.height-1
+                    dash_offset: 5
+                    dash_length: 3
+            Label:
+                text_size: self.size
+                halign: 'left'
+                valign: 'middle'
+                pos_hint:{ 'center_x': .5,'center_y': .8}
+                #padding: 50.0,50.0 #左右、上下
+                #spacing: 50.0,50.0 #pixcel        
+                text: 'Stock Card'
+                #size_hint_y: None
+                size_hint_y: 0.3
+                size_hint_x: 0.5
+                color: 1, 0, 0, 1#text color
+                bold: True
+                font_size: 30
+                canvas.before:
+                    Color:
+                        rgba: hex('#ffffff')
+                    Line:
+                        rectangle: self.x+1,self.y+1,self.width-1,self.height-1
+                        dash_offset: 5
+                        dash_length: 3
+                    #Rectangle:
+                    #    pos: self.pos
+                    #    size: self.size
+            Button:
+                text:'Button'
+                size_hint_y: 0.3
+                size_hint_x: 0.1
+                pos_hint:{ 'center_x': .5,'center_y': .5}
+                canvas.before:
+                    Color:
+                        rgba: hex('#ffffff')
+                    Line:
+                        rectangle: self.x+1,self.y+1,self.width-1,self.height-1
+                        dash_offset: 5
+                        dash_length: 3
+                    #Rectangle:
+                    #    pos: self.pos
+                    #    size: self.size
+
+        BoxLayout:
+            orientation: 'vertical'
+            size_hint_y: 0.3
+            Label:
+                text: 'Label 2'
+                size_hint_y: 0.3
+                size_hint_x: 1.0
+                pos_hint:{ 'center_x': .5,'center_y': .5}
+                #padding: 0.0, 25.0 #左右、上
+                spacing: 150.0, 150.0#       
+                color: 1, 0, 0, 1#text color
+                bold: True
+                font_size: 30
+                canvas.before:
+                    Color:
+                        rgba: 1, 1, 1, 1
+                    Line:
+                        rectangle: self.x+1,self.y+1,self.width-1,self.height-1
+                        dash_offset: 5
+                        dash_length: 3
+                    #Rectangle:
+                    #    pos: self.pos
+                    #    size: self.size
+        BoxLayout:
+            orientation: 'vertical'
+            size_hint_y: 0.3
+            Label:
+                text: 'Label 3'
+                size_hint_y: 0.3
+                size_hint_x: 0.8
+                color: 1, 0, 0, 1#text color
+                bold: True
+                font_size: 30
+                spacing: 150.0, 150.0#    
+                canvas.before:
+                    Color:
+                        rgba: 1, 1, 1, 1
+                    Line:
+                        rectangle: self.x+1,self.y+1,self.width-1,self.height-1
+                        dash_offset: 5
+                        dash_length: 3
+                    #Rectangle:
+                    #    pos: self.pos
+                    #    size: self.size      
 
         
 """
